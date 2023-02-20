d68bdd704febaf8c59812cff5d4ca27cb4c8eea4
Himanshu <himanshu16315@gmail.com>
2023-02-20 16:53:23 -0500

    Added products 👕

---
+++ components/product/Product.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Product = ({ item }) => {
+  return (
+    <div className=" flex flex-col justify-center items-center hover:cursor-pointer duration-300">
+      <div className="group relative duration-300">
+        <img src={item.image} alt="" className="w-full h-80" />
+        <div className="hidden absolute h-full w-full top-0 left-0 group-hover:flex justify-center items-center duration-300 bg-gray-100/40">
+          <button className="w-3/4 py-6 px-2 bg-red-100 bg-white/90 uppercase tracking-wide duration-300 hover:bg-black/90 hover:text-gray-100">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between w-full p-2">
+        <h1>{item.title}</h1>
+        <div className="flex flex-row space-x-2 items-center">
+          <h1>${item.price}</h1>
+          <button
+            className="text-pink-700"
+            onClick={() => console.log(item.title)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- components/trending/Trending.jsx
+++ components/trending/Trending.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { products } from "../../utils/products";
+import Product from "../product/Product";
 
 const Trending = () => {
   return (
@@ -8,22 +9,9 @@ const Trending = () => {
       <div className="h-full w-full flex justify-center py-6">
         <h1 className="text-5xl font-Gruppo uppercase">Trending Now</h1>
       </div>
-      <div className="text-black grid grid-cols-2 gap-8 md:grid-cols-4 py-12 px-6">
+      <div className="text-black grid grid-cols-2 gap-8 lg:grid-cols-3 xl:grid-cols-4 py-12 px-6">
         {products.map((item) => (
-          <div className="group flex flex-col justify-center items-center hover:cursor-pointer duration-300">
-            <div className="relative duration-300">
-              <img src={item.image} alt="" className="w-full h-80" />
-              <div className="hidden absolute h-full w-full top-0 left-0 group-hover:flex justify-center items-center duration-300 bg-gray-100/40">
-                <button className="w-3/4 py-6 px-2 bg-red-100 bg-white/90 uppercase tracking-wide duration-300 hover:bg-black/90 hover:text-gray-100">
-                  Add to Cart
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-row justify-between w-full p-2">
-              <h1>{item.title}</h1>
-              <h1>{item.price}</h1>
-            </div>
-          </div>
+          <Product item={item} />
         ))}
       </div>
     </div>
