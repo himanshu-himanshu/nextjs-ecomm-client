2d275af1bcf2ef3bc793aae51cd4c02a4e5304ea
Himanshu <himanshu16315@gmail.com>
2023-02-22 14:19:51 -0500

    Added men and women pages 🎯

--- components/navbar/Links.jsx
+++ components/navbar/Links.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 const Links = () => {
   return (
     <div className="flex flex-row space-x-4 lg:space-x-8 uppercase tracking-wider text-sm font-light">
-      <span className="hover:cursor-pointer hover:text-gray-500">Home</span>
-      <span className="hover:cursor-pointer hover:text-gray-500">Trending</span>
-      <span className="hover:cursor-pointer hover:text-gray-500">
+      <Link className="hover:cursor-pointer hover:text-gray-500" href="/">
+        Home
+      </Link>
+      <Link
+        className="hover:cursor-pointer hover:text-gray-500"
+        href="/trending"
+      >
+        Trending
+      </Link>
+      <Link
+        className="hover:cursor-pointer hover:text-gray-500"
+        href="/all-products"
+      >
         All Products
-      </span>
+      </Link>
     </div>
   );
 };
--- components/sections/GenderSection.jsx
+++ components/sections/GenderSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const GenderSection = () => {
   return (
@@ -7,15 +8,21 @@ const GenderSection = () => {
       <div className="h-full w-full flex flex-row justify-center space-x-4">
         <div className="h-full w-1/2 border flex flex-col justify-center items-center space-y-12 bg-gradient-to-l from-gray-100 to-gray-300">
           <h1 className="text-4xl">Men</h1>
-          <button className="mt-6 font-bold px-4 md:px-8 py-4 border border-secondary hover:bg-secondary text-primary duration-200 text-xl md:text-2xl font-Gruppo tracking-wider w-[150px] md:w-[200px] hover:-translate-y-1 hover:shadow-2xl bg-secondary/80">
+          <Link
+            className="mt-6 font-bold px-4 md:px-8 py-4 border border-secondary hover:bg-secondary text-primary duration-200 text-xl md:text-2xl font-Gruppo tracking-wider w-[150px] md:w-[200px] hover:-translate-y-1 hover:shadow-2xl bg-secondary/80"
+            href="/men-products"
+          >
             Shop Now
-          </button>
+          </Link>
         </div>
         <div className="h-full w-1/2 border flex flex-col justify-center items-center space-y-12 bg-gradient-to-r from-gray-100 to-gray-300">
           <h1 className="text-4xl">Women</h1>
-          <button className="mt-6 font-bold px-4 md:px-8 py-4 border border-secondary hover:bg-secondary text-primary duration-200 text-xl md:text-2xl font-Gruppo tracking-wider w-[150px] md:w-[200px] hover:-translate-y-1 hover:shadow-2xl bg-secondary/80">
+          <Link
+            className="mt-6 font-bold px-4 md:px-8 py-4 border border-secondary hover:bg-secondary text-primary duration-200 text-xl md:text-2xl font-Gruppo tracking-wider w-[150px] md:w-[200px] hover:-translate-y-1 hover:shadow-2xl bg-secondary/80"
+            href="women-products"
+          >
             Shop Now
-          </button>
+          </Link>
         </div>
       </div>
     </div>
