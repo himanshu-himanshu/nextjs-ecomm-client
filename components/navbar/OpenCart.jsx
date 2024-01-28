d7502cb080f4dbb6aa8fcf6aedebb77c5c55b549
Himanshu <himanshu16315@gmail.com>
2024-01-28 14:49:34 -0500

    Cart refactored 🐞

--- components/navbar/OpenCart.jsx
+++ components/navbar/OpenCart.jsx
@@ -6,8 +6,17 @@ import Cart from "./Cart";
 import { products } from "../../utils/products";
 import cross from "../../assets/close.png";
 import { useDispatch, useSelector } from "react-redux";
-import { ShoppingCartIcon } from "@heroicons/react/24/outline";
-import { FaceFrownIcon } from "@heroicons/react/24/outline";
+import {
+  ShoppingCartIcon,
+  MinusIcon,
+  PlusIcon,
+  MinusCircleIcon,
+  PlusCircleIcon,
+  TrashIcon,
+  BoltIcon,
+  StarIcon,
+  CheckBadgeIcon,
+} from "@heroicons/react/24/outline";
 
 const OpenCart = ({ handleCloseCart, handleOpenCart }) => {
   const dispatch = useDispatch();
@@ -29,14 +38,15 @@ const OpenCart = ({ handleCloseCart, handleOpenCart }) => {
         className="absolute h-full bg-white top-0 right-0 w-full sm:min-w-[500px] sm:w-1/3 overflow-hidden z-40"
       >
         <div className="flex flex-row justify-between items-center px-6 py-6 h-[10vh] border-b">
+          <Cart handleOpenCart={handleOpenCart} />
+
+          <h1 className="font-Borui tracking-wide text-xl">Your Cart</h1>
           <div onClick={() => handleCloseCart()}>
             <CloseButton />
           </div>
-          <h1 className="font-Borui tracking-wide">Your Cart</h1>
-          <Cart handleOpenCart={handleOpenCart} />
         </div>
         <div className="h-[90vh]">
-          <div className="flex flex-col h-4/6 justify-center items-center">
+          <div className="flex flex-col h-[75%] justify-center items-center">
             {/** If there is no Item in cart render the div below */}
             {items.length === 0 && (
               <div className="flex flex-col items-center justify-center space-y-4">
@@ -58,19 +68,29 @@ const OpenCart = ({ handleCloseCart, handleOpenCart }) => {
                     className="group flex flex-row items-center duration-300 p-4 border-b border-gray-200"
                     key={item.id}
                   >
-                    <div className="relative duration-300 w-1/2">
+                    <div className="relative duration-300 w-1/3">
                       <img src={item.image} alt="" className="w-24 h-24" />
                     </div>
-                    <div className="flex flex-col justify-between w-1/2 p-2 space-y-4">
+                    <div className="flex flex-col justify-between w-2/3 p-2 space-y-4">
                       <div className="flex flex-row justify-between items-center">
-                        <h1>{item.title}</h1>
+                        <h1 className="font-Gruppo font-extrabold text-lg">
+                          {item.title}
+                        </h1>
                       </div>
                       <div className="flex flex-row justify-between items-center">
-                        <h1 className="font-bold">${item.price}</h1>
-                        <span className="flex flex-row items-center justify-center p-2 space-x-1">
-                          <Image src={cross} alt="" className="w-3 h-3" />
-                          <span className="text-lg">3</span>
+                        <h1 className="font-medium text-xl font-Gruppo">
+                          ${item.price}
+                        </h1>
+                        <span className="flex flex-row items-center justify-center px-2 space-x-4 border">
+                          <MinusIcon className="h-4 w-4 text-gray-500 hover:cursor-pointer" />
+                          <span className="text-lg font-Gruppo font-extrabold">
+                            3
+                          </span>
+                          <PlusIcon className="h-4 w-4 text-gray-500 hover:cursor-pointer" />
                         </span>
+                        <div>
+                          <TrashIcon className="h-6 w-6 text-pink-800 hover:cursor-pointer" />
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -78,14 +98,19 @@ const OpenCart = ({ handleCloseCart, handleOpenCart }) => {
               </div>
             )}
           </div>
-          <div className="w-full h-2/6 p-4 bg-secondary">
-            <h1 className="my-8 text-lg uppercase tracking-wide flex justify-between font-Gruppo font-extrabold">
-              <span>Subtotal</span>
-              <span>$400</span>
+          <div className="w-full h-[25%] p-4 bg-secondary">
+            <h1 className="text-[14px] tracking-wide flex justify-end items-end font-Gruppo mt-4 mb-6">
+              <span>Shipping & taxes calculated at checkout</span>
             </h1>
-            <button className="w-full font-bold px-8 py-4 border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-2xl font-Gruppo tracking-wider">
-              Checkout
-            </button>
+            <div className="w-full font-bold uppercase px-6 py-5 bg-primary/90 border border-primary text-secondary hover:bg-primary hover:text-white duration-200 font-Gruppo flex justify-center items-center space-x-2 hover:cursor-pointer shadow-sm">
+              <button className="text-lg tracking-widest uppercase">
+                Checkout
+              </button>
+              <CheckBadgeIcon className="h-5 w-5" />
+              <span className="text-lg font-semibold tracking-wider">
+                $400.00 CAD
+              </span>
+            </div>
           </div>
         </div>
       </motion.div>
--- components/product/Product.jsx
+++ components/product/Product.jsx
@@ -1,72 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   addItemToCart,
   calculateTotalItemsQuantity,
 } from "../../redux/features/cart/cartSlice";
+import { toast, Toaster } from "react-hot-toast";
+import { ThreeDots } from "react-loader-spinner";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
 
 const Product = ({ item }) => {
+  const [addingToCart, setAddingToCart] = useState(false);
   const dispatch = useDispatch();
 
-  return (
-    <div className=" flex flex-col justify-center items-center hover:cursor-pointer duration-300 border border-gray-100/70 rounded-sm">
-      <div className="group relative duration-300 px-2 py-4">
-        <img src={item.image} alt="" className="w-full h-80" />
-        <div className="hidden absolute h-full w-full top-0 left-0 group-hover:flex justify-center items-center duration-300 bg-gray-100/40">
-          <button
-            className="w-[200px] py-6 px-2 bg-gray-100 uppercase tracking-wide duration-300 hover:bg-black/90 hover:text-gray-100"
-            onClick={() => {
-              dispatch(addItemToCart(item));
-            }}
-          >
-            Add to Cart
-          </button>
+  const addItemToCartFunction = () => {
+    setAddingToCart(true);
+    setTimeout(() => {
+      dispatch(addItemToCart(item));
+      setAddingToCart(false);
+      toast.custom(
+        <div className="px-6 py-6 bg-primary/50 text-secondary font-Borui rounded-xl flex flex-row space-x-4 shadow-sm duration-300">
+          <span className="tracking-wide"> Item added to cart</span>
+          <ShoppingBagIcon class="h-6 w-6 text-secondary" />
         </div>
-      </div>
-      <div className="w-full px-2 pb-1 flex flex-row justify-between items-center">
-        <h1 className="font-semibold text-gray-700 text-sm">{item.brand}</h1>
-        <span className="flex flex-row items-center space-x-2">
-          <span className="text-gray-600">{item.rating}</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5 text-yellow-400"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </span>
-      </div>
-      <div className="flex flex-row justify-between w-full px-2 pb-2">
-        <h1 className="text-gray-700">{item.title}</h1>
-        <div className="flex flex-row space-x-2 items-center">
-          <h1 className="text-gray-700">${item.price}</h1>
-          <button
-            className="text-pink-700"
-            onClick={() => console.log(item.title)}
-          >
+      );
+    }, 500);
+  };
+
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 700,
+        }}
+      />
+      <div className=" flex flex-col justify-center items-center hover:cursor-pointer duration-300 border border-gray-100/70 rounded-sm">
+        <div className="group relative duration-300 px-2 py-4">
+          <img src={item.image} alt="" className="w-full h-80" />
+          <div className="hidden absolute h-full w-full top-0 left-0 group-hover:flex justify-center items-center duration-300 bg-gray-100/40">
+            {!addingToCart && (
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-[200px] py-6 px-2 bg-gray-100 uppercase tracking-wide duration-300 hover:bg-black/90 hover:text-gray-100"
+                onClick={() => {
+                  addItemToCartFunction();
+                }}
+              >
+                Add to Cart
+              </motion.button>
+            )}
+            {addingToCart && (
+              <button className="w-[200px] py-3 px-2 bg-gray-100 uppercase tracking-wide duration-300 flex justify-center items-center">
+                <ThreeDots
+                  visible={addingToCart}
+                  height="44"
+                  width="44"
+                  color="#113034"
+                  radius="24"
+                  ariaLabel="three-dots-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="w-full px-2 pb-1 flex flex-row justify-between items-center">
+          <h1 className="font-semibold text-gray-700 text-sm">{item.brand}</h1>
+          <span className="flex flex-row items-center space-x-2">
+            <span className="text-gray-600">{item.rating}</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+              fill="currentColor"
+              className="w-5 h-5 text-yellow-400"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                fillRule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                clipRule="evenodd"
               />
             </svg>
-          </button>
+          </span>
+        </div>
+        <div className="flex flex-row justify-between w-full px-2 pb-2">
+          <h1 className="text-gray-700">{item.title}</h1>
+          <div className="flex flex-row space-x-2 items-center">
+            <h1 className="text-gray-700">${item.price}</h1>
+            <button
+              className="text-pink-700"
+              onClick={() => console.log(item.title)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
